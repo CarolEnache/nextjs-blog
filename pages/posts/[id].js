b1add2c4fdc5a4698e9332df4917f64469a0ae51
CarolEnache <enache.carol@gmail.com>
2020-08-16 20:41:35 +0100

    Implement getStaticProps  for Dynamic Routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,5 @@
 import Layout from '../../components/layout'
-import { getAllPostIds } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -10,6 +10,28 @@ export async function getStaticPaths() {
   }
 }
 
-export default function Post() {
-  return <Layout>...</Layout>
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+
+  console.log('sdfsdfsdfsdfsdfsf')
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+
+export default function Post({ postData }) {
+  console.log(postData)
+
+  return (
+    <Layout>
+    {postData.title}
+    <br />
+    {postData.id}
+    <br />
+    {postData.date}
+  </Layout>
+  )
 }
